64dc95743bd01c1582ca41081b9accbfa94e9f3f
IvanKharkivskyi1 <ikcomua@gmail.com>
2024-11-26 17:21:09 +0200

    feat: initial commit

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,12 @@
 import { createTheme} from '@mui/material';
 
 export const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#1976d2',
-      },
+  palette: {
+    primary: {
+      main: '#1976d2',
     },
-    typography: {
-        fontFamily: `'Roboto', 'Arial', sans-serif`, // Replace with your custom font name
-      },
+  },
+  typography: {
+    fontFamily: `'Roboto', 'Arial', sans-serif`,
+  },
 });
